68685c58b5ea34fee9c8921cf4e52d634d7ff6e8
Willow Rocha <willow_grs@hotmail.com>
2024-05-27 17:27:27 -0300

    Implements AWS Swagger Converter

---
+++ src/converter/index.js
@@ -0,0 +1,206 @@
+import React from "react";
+import { useState } from "react";
+import { Button, Wrapper } from "./styles";
+import Input from "../input";
+import InputTextarea from "../textarea";
+import { each, find, get, replace, set, size, toUpper } from "lodash";
+
+const ConverterComponent = (props) => {
+  const [awsAccountId, setAwsAccountId] = useState("");
+  const [awsInternalUrl, setAwsInternalUrl] = useState("");
+  const [awsDefaultRegion, setAwsDefaultRegion] = useState("");
+  const [awsVPCLinkId, setAwsVPCLinkId] = useState("");
+  const [awsHost, setAwsHost] = useState("");
+  const [awsBasePath, setAwsBasePath] = useState("");
+  const [jsonInput, setJsonInput] = useState("");
+  const [jsonOutput, setJsonOutput] = useState("");
+
+  const convertData = () => {
+    const errors = [];
+    if (size(awsAccountId) === 0) {
+      errors.push("awsAccountId");
+    }
+    if (size(awsInternalUrl) === 0) {
+      errors.push("awsInternalUrl");
+    }
+    if (size(awsDefaultRegion) === 0) {
+      errors.push("awsDefaultRegion");
+    }
+    if (size(awsVPCLinkId) === 0) {
+      errors.push("awsVPCLinkId");
+    }
+    if (size(jsonInput) === 0) {
+      errors.push("jsonInput");
+    }
+    if (size(errors) > 0) {
+      alert("Required: " + errors.join(", "));
+      return;
+    }
+
+    const data = JSON.parse(jsonInput);
+
+    // Adds host
+    if (size(awsHost) > 0) {
+      set(data, "host", awsHost);
+    }
+
+    // Adds basePath
+    if (size(awsBasePath) > 0) {
+      set(data, "basePath", awsBasePath);
+    }
+
+    // Adds schemes
+    set(data, "schemes", ["https"]);
+
+    each(data.paths, (path, endpoint) => {
+      let hasOptions = false;
+      const parameters = [];
+
+      // Adds API Gateway configuration
+      each(path, (method, type) => {
+        if (toUpper(type) === "OPTIONS") {
+          hasOptions = true;
+        } else {
+          set(method, "x-amazon-apigateway-integration", {
+            connectionId: awsVPCLinkId,
+            connectionType: "VPC_LINK",
+            httpMethod: toUpper(type),
+            type: "http_proxy",
+            uri: `${awsInternalUrl}${endpoint}`,
+          });
+
+          if (method.parameters) {
+            each(method.parameters, (param) => {
+              if (
+                param.in === "path" &&
+                find(parameters, (it) => it.name === param.name) === undefined
+              ) {
+                parameters.push(param);
+              }
+            });
+          }
+        }
+      });
+
+      // Adds OPTIONS method
+      if (!hasOptions) {
+        set(path, "options", {
+          consumes: ["application/json"],
+          parameters: parameters,
+          responses: {
+            200: {
+              description: "200 response",
+              headers: {
+                "Access-Control-Allow-Origin": { type: "string" },
+                "Access-Control-Allow-Methods": { type: "string" },
+                "Access-Control-Allow-Headers": { type: "string" },
+              },
+            },
+          },
+          "x-amazon-apigateway-integration": {
+            responses: {
+              default: {
+                statusCode: "200",
+                responseParameters: {
+                  "method.response.header.Access-Control-Allow-Methods":
+                    "OPTIONS",
+                  "method.response.header.Access-Control-Allow-Headers":
+                    "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+                  "method.response.header.Access-Control-Allow-Origin": "*",
+                },
+              },
+            },
+            requestTemplates: {
+              "application/json": '{"statusCode": 200}',
+            },
+            passthroughBehavior: "when_no_match",
+            type: "mock",
+          },
+        });
+      }
+    });
+
+    // Replaces Account ID and AWS Timezone
+    const authorizer = get(
+      data,
+      "components.securitySchemes.APICustomAuthorizationHeader.x-amazon-apigateway-authorizer"
+    );
+
+    let credentials = get(authorizer, "authorizerCredentials");
+    credentials = replace(credentials, "aws-account-id", awsAccountId);
+    set(authorizer, "authorizerCredentials", credentials);
+
+    let uri = get(authorizer, "authorizerUri");
+    uri = replace(uri, "aws-account-id", awsAccountId);
+    uri = replace(uri, "aws-default-region", awsDefaultRegion);
+    set(authorizer, "authorizerUri", uri);
+
+    setJsonOutput(JSON.stringify(data));
+  };
+
+  return (
+    <Wrapper>
+      <Input
+        name="awsAccountId"
+        label="AWS Account ID"
+        type="text"
+        value={awsAccountId}
+        onChange={(e) => setAwsAccountId(e.target.value)}
+      />
+      <Input
+        name="awsDefaultRegion"
+        label="AWS Default Region"
+        type="text"
+        value={awsDefaultRegion}
+        onChange={(e) => setAwsDefaultRegion(e.target.value)}
+      />
+      <Input
+        name="awsHost"
+        label="AWS application Host/Address"
+        type="text"
+        value={awsHost}
+        onChange={(e) => setAwsHost(e.target.value)}
+      />
+      <Input
+        name="awsBasePath"
+        label="AWS application basePath (with slash)"
+        type="text"
+        value={awsBasePath}
+        onChange={(e) => setAwsBasePath(e.target.value)}
+      />
+      <Input
+        name="awsInternalUrl"
+        label="AWS Internal APP URL"
+        type="text"
+        value={awsInternalUrl}
+        onChange={(e) => setAwsInternalUrl(e.target.value)}
+      />
+      <Input
+        name="awsVPCLinkId"
+        label="AWS VPN Link ID"
+        type="text"
+        value={awsVPCLinkId}
+        onChange={(e) => setAwsVPCLinkId(e.target.value)}
+      />
+      <InputTextarea
+        name="jsonInput"
+        label="Json Input"
+        type="textarea"
+        value={jsonInput}
+        onChange={(e) => setJsonInput(e.target.value)}
+      />
+
+      <Button onClick={convertData}>Convert</Button>
+
+      <InputTextarea
+        name="jsonOutput"
+        label="Json Output"
+        type="textarea"
+        value={jsonOutput}
+        onChange={(e) => setJsonOutput(e.target.value)}
+      />
+    </Wrapper>
+  );
+};
+
+export default ConverterComponent;
